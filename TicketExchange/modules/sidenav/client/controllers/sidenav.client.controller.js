91ebf90985a23d95bab9193836a2ad72af7136a2
Sharath Narayan <phsharathnarayan@gmail.com>
2016-02-14 18:04:44 -0600

    First Commit

---
+++ TicketExchange/modules/sidenav/client/controllers/sidenav.client.controller.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+    
+    SidenavCtrl.$inject = ['$scope','$window','Authentication'];
+    
+    function SidenavCtrl($scope, $window, Authentication) {
+        $scope.menu = [
+            {
+                link : 'home.business',
+                title: 'Business',
+                icon: 'dashboard'
+            },
+            {
+                link : 'home.help',
+                title: 'Help',
+                icon: 'group'
+            },
+            {
+                link : 'home.settings',
+                title: 'Settings',
+                icon: 'message'
+            }
+        ];
+        $scope.admin = [
+            {
+                link : '',
+                title: 'Trash',
+                icon: 'delete'
+            },
+            {
+                link : 'showListBottomSheet($event)',
+                title: 'Settings',
+                icon: 'settings'
+            }
+        ];
+        $scope.authentication = Authentication;
+        $scope.signOut = function () {
+            $window.open('/api/auth/signout', '_self');
+        }
+    }
+    angular.module('sidenav.landing').controller('SidenavCtrl', SidenavCtrl);
+})();
