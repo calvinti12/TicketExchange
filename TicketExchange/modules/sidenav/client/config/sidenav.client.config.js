0c7ee813575fb7c6b537c53b457f754e152e9585
Sharath Narayan <phsharathnarayan@gmail.com>
2016-02-16 20:54:58 -0600

    sidenav without divider

---
+++ TicketExchange/modules/sidenav/client/config/sidenav.client.config.js
@@ -0,0 +1,38 @@
+﻿(function () { 
+    'use strict';
+    angular.module('sidenav.landing').run(['Menus',
+        function (Menus) {
+            // Add the sidenav items
+            Menus.addMenuItem('sidenav', {
+                title: 'Business',
+                state: 'home.business',
+                icon: 'dashboard',
+                roles: ['*']
+            });
+            Menus.addMenuItem('sidenav', {
+                title: 'Help',
+                state: 'home.help',
+                icon: 'dashboard',
+                roles: ['*']
+            });
+            Menus.addMenuItem('sidenav', {
+                title: 'Settings',
+                state: 'home.settings',
+                icon: 'dashboard',
+                roles: ['*']
+            });
+            
+            //Add the admin panel items
+            Menus.addMenuItem('sidenav', {
+                title: 'Admin Panel 1',
+                icon: 'dashboard',
+                roles: ['admin']
+            });
+            Menus.addMenuItem('sidenav', {
+                title: 'Admin Panel 2',
+                icon: 'dashboard',
+                roles: ['admin']
+            });
+        }
+    ]);
+})();
