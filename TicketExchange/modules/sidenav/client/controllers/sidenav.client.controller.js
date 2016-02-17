c76b2814e098cbe1f3003769deae64db2ac3bdba
Sharath Narayan <phsharathnarayan@gmail.com>
2016-02-16 21:49:39 -0600

    Removed comments

--- TicketExchange/modules/sidenav/client/controllers/sidenav.client.controller.js
+++ TicketExchange/modules/sidenav/client/controllers/sidenav.client.controller.js
@@ -4,36 +4,6 @@
     SidenavCtrl.$inject = ['$scope','$window','Authentication', 'Menus'];
     
     function SidenavCtrl($scope, $window, Authentication, Menus) {
-        //$scope.menu = [
-        //    {
-        //        link : 'home.business',
-        //        title: 'Business',
-        //        icon: 'dashboard'
-        //    },
-        //    {
-        //        link : 'home.help',
-        //        title: 'Help',
-        //        icon: 'group'
-        //    },
-        //    {
-        //        link : 'home.settings',
-        //        title: 'Settings',
-        //        icon: 'message'
-        //    }
-        //];
-        //$scope.admin = [
-        //    {
-        //        link : '',
-        //        title: 'Trash',
-        //        icon: 'delete'
-        //    },
-        //    {
-        //        link : 'showListBottomSheet($event)',
-        //        title: 'Settings',
-        //        icon: 'settings'
-        //    }
-        //];
-        // Get the topbar menu
         $scope.menu = Menus.getMenu('sidenav');
         $scope.authentication = Authentication;
         $scope.signOut = function () {
